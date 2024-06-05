54b2a68ad29bc696b18ab38b0923fa31b1957349
BinuMDX <binarigpv@gmail.com>
2024-06-05 17:43:40 +0530

    add login functionality

--- ASPIdentity/Controllers/AccountController.cs
+++ ASPIdentity/Controllers/AccountController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPIdentity.Models;
+using ASPIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPIdentity.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> _signInManager)
+        {
+            this._signInManager = _signInManager;
+        }
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginVM model)
+        {
+            if(ModelState.IsValid)
+            {
+                //login
+                var result = await _signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
+                
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid login attempt");
+                return View(model);
+            }
             return View();
         }
 
